22ea8da2b8a2c867d626672763ad9f1a0fc88147
Wictorgirardi <wictor.girardi@gmail.com>
2022-07-19 11:48:27 -0300

    feat: Create router
    Co-authored-by: pedro-rodiguero<pedro.m.rodiguero@gmail.com>
    Co-authored-by: DanRocha18 <daniel.rocha1207@gmail.com>"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,65 @@
 
 import GlobalStyle from '~assets/styles/global'
 import truck from '~assets/images/truck.gif'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
-    <>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+          <>
      <GlobalStyle />
       <img
         src={truck}
         style={{ height: 200, position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)' }}
-      />
-    </>
+       />
+          </>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
   )
 }
 
