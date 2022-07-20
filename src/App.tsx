c56db7205b0082d270ed0b8053840fc1f7507a77
AlefMemTav <190115548@aluno.unb.br>
2022-07-20 08:13:00 -0300

    Updated to use Routes and add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,23 @@
 
 import GlobalStyle from '~assets/styles/global'
 import truck from '~assets/images/truck.gif'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+
+import Home from './pages/Home'
 import Login from 'pages/Login'
 import Register from 'pages/Register'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/">
-          <>
-     <GlobalStyle />
-      <img
-        src={truck}
-        style={{ height: 200, position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)' }}
-       />
-          </>
-          </Route>
-          <Route path="/login">
-           <Login/>
-          </Route>
-          <Route path="/register">
-          <Register/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
