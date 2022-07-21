3d0e2ab9ac794ce3374db99edb4a1b360c422fe0
Bruno Henrique <bh.bsb2011@hotmail.com>
2022-07-21 17:13:19 -0300

    componente UserCard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,19 @@ import Typography from '@mui/material/Typography'
 import { Avatar } from '@mui/material'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 
+const UserCard = () => {
+  return (
+    <Box display='flex'>
+      <Avatar />
+      <Box>
+        <Typography>Fulano de tal</Typography>
+        <Typography>Administrador</Typography>
+      </Box>
+      <ArrowDropDownIcon/>
+    </Box>
+  )
+}
+
 export default function Header() {
   return (
       <AppBar position="static" sx={{ color: 'white'}}>
@@ -14,6 +27,7 @@ export default function Header() {
           Titulo do header
           </Typography>
         </Toolbar>
+        <UserCard />
       </AppBar>
   )
 }
