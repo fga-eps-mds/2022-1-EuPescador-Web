19d9288ea68a8d2c4e05c0fa930877d90428df17
verabelucia <verabelucia@gmail.com>
2022-07-25 15:27:39 -0300

    Parameterize FishCard component

--- src/components/FishCard/FishCard.tsx
+++ src/components/FishCard/FishCard.tsx
@@ -1,10 +1,21 @@
-import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
 import ButtonBase from '@mui/material/ButtonBase'
 import AllOutIcon from '@mui/icons-material/AllOut'
+
+type Fish = {
+  name: string;
+  size: string;
+  weigth: string;
+  place: string;
+  imageUrl: string;
+}
+
+type FishCardProps = {
+  fish: Fish;
+}
 
 const Img = styled('img')({
   margin: 'auto',
@@ -15,7 +26,7 @@ const Img = styled('img')({
 
 })
 
-export default function fishcard() {
+export function FishCard(props: FishCardProps) {
   return (
     <Paper
       sx={{
@@ -23,31 +34,30 @@ export default function fishcard() {
         margin: 0,
         maxWidth: 330,
         flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
       }}
     >
       <Grid container spacing={2}>
         <Grid item>
           <ButtonBase sx={{ width: 170, height: 150 }}>
-            <Img alt="complex" src='https://www.istoedinheiro.com.br/wp-content/uploads/sites/17/2022/02/tilapia-e1645622421681.jpg' />
+            <Img alt="complex" src={props.fish.imageUrl} />
           </ButtonBase>
         </Grid>
         <Grid item xs={12} sm container>
           <Grid item xs container direction="column" spacing={2}>
             <Grid item xs>
               <Typography gutterBottom variant="h5" component="div">
-                Tilapia azul
+                {props.fish.name}
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                122 cm
+                {props.fish.size}
               </Typography>
               <Typography variant='body2' color='text.secondary'>
-                Pantanal Brasil
+                {props.fish.place}
               </Typography>
               <Typography variant='body2' color='text.secondary'>
-                Até 120 KG
-              </Typography >
+                {props.fish.weigth}
+              </Typography>
             </Grid>
             <Grid container justifyContent='flex-end'>
               <AllOutIcon />
