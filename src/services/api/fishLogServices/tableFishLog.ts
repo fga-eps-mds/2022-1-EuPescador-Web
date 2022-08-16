88568a3b66fd5e40ab15eb9f5f97fbca20a622ae
DanRocha18 <daniel.rocha1207@gmail.com>
2022-08-16 09:47:40 -0300

    Importing components and services

---
+++ src/services/api/fishLogServices/tableFishLog.ts
@@ -0,0 +1,9 @@
+import React from "react";
+import TableComponent from "~components/Table";
+import { GetAllFishLogs } from "./getAllLogs";
+import { UpdateFishLog } from "./updateFishLog";
+import { DeleteFishLog } from "./deleteFishLog";
+import { ExportFishLogs } from "./exportFishLog";
+import Header from "~components/Header";
+import Sidebar from "~components/Sidebar";
+
