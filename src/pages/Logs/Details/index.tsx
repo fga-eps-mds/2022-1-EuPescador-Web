81aecb49af54552774b312d60310fe61c8c061e9
Bruno Henrique <bh.bsb2011@hotmail.com>
2022-08-23 21:48:44 -0300

    buscar o dados do peixe e mostar imagem e mapa

--- src/pages/Logs/Details/index.tsx
+++ src/pages/Logs/Details/index.tsx
@@ -1,8 +1,50 @@
-import { Grid, Box, Button, TextField, Switch, FormGroup, FormControlLabel, Typography } from '@mui/material'
-import Header from '~components/Header'
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { Grid, Box, TextField, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Header, { UserProps } from '~components/Header'
 import Sidebar from '../../../components/Sidebar'
+import { GetOneFishLog } from '../../../services/api/fishLogServices/getOneFishLog'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+export interface FishLogProps {
+  coordenates: {
+    latitude: number
+    longitude: number
+  }
+  family: string
+  group: string
+  largeGroup: string
+  name: string
+  photo: string | null
+  reviewed: boolean
+  species: string
+  length: number
+  weight: number
+}
 
 export default function LogsDetails() {
+  const [log, setLog] = useState({} as FishLogProps)
+  const { id } = useParams()
+
+  const getLog = async (logId: string) => {
+    const user: UserProps = JSON.parse(localStorage.getItem('UserData')) as UserProps
+    const data = await GetOneFishLog(logId, user.token)
+    console.log(data.photo)
+    setLog(data)
+  }
+
+  const loadingMessage = 'Carregando...'
+
+  useEffect(() => {
+    if (id) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      getLog(id)
+    }
+  }, [id])
+
   return (
     <Grid container>
       <Grid item xs={1}>
@@ -10,15 +52,14 @@ export default function LogsDetails() {
       </Grid>
       <Grid item xs={11}>
         <Header title="Detalhes do Log"></Header>
-        <Box sx={{ mt: 1, display:'flex' }}>
+        <Box sx={{ mt: 1, display: 'flex' }}>
           <Box sx={{ mt: 1, width: '50%' }}>
             <TextField
               margin="normal"
-              required
               fullWidth
               label="Nome"
               name="name"
-              autoFocus
+              value={log.name || loadingMessage}
               InputLabelProps={{
                 style: { color: '#111111' },
               }}
@@ -27,17 +68,15 @@ export default function LogsDetails() {
                   borderRadius: '8px',
                   color: '#111111',
                   width: '500px',
-
                 },
               }}
             />
             <TextField
               margin="normal"
-              required
               fullWidth
               label="Classe"
               name="largeGroup"
-              autoFocus
+              value={log.largeGroup || loadingMessage}
               InputLabelProps={{
                 style: { color: '#111111' },
               }}
@@ -46,17 +85,15 @@ export default function LogsDetails() {
                   borderRadius: '8px',
                   color: '#111111',
                   width: '500px',
-
                 },
               }}
             />
             <TextField
               margin="normal"
-              required
               fullWidth
               label="Ordem"
               name="group"
-              autoFocus
+              value={log.group || loadingMessage}
               InputLabelProps={{
                 style: { color: '#111111' },
               }}
@@ -65,17 +102,15 @@ export default function LogsDetails() {
                   borderRadius: '8px',
                   color: '#111111',
                   width: '500px',
-
                 },
               }}
             />
             <TextField
               margin="normal"
-              required
               fullWidth
               label=" Espécie"
               name="species"
-              autoFocus
+              value={log.species || loadingMessage}
               InputLabelProps={{
                 style: { color: '#111111' },
               }}
@@ -84,59 +119,61 @@ export default function LogsDetails() {
                   borderRadius: '8px',
                   color: '#111111',
                   width: '500px',
-
                 },
               }}
             />
-            <Box sx={{ mt: 1 , display: 'flex' , width: '20%' }}>
-            <TextField
-              margin="normal"
-              required
-              sx={{width: '50%'}}
-              label="Massa"
-              name="weight"
-              autoFocus
-              InputLabelProps={{
-                style: { color: '#111111'},
-              }}
-              InputProps={{
-                style: {
-                  borderRadius: '8px',
-                  color: '#111111',
-                  width: '80%',
-
-                },
-              }}
-            /><TextField
-              margin="normal"
-              required
-              sx={{width: '50%'}}
-              label="Tamanho"
-              name="length"
-              autoFocus
-              InputLabelProps={{
-                style: { color: '#111111'},
-              }}
-              InputProps={{
-                style: {
-                  borderRadius: '8px',
-                  color: '#111111',
-                  width: '80%',
-
-                },
-              }}
-            />
+            <Box sx={{ mt: 1, display: 'flex', width: '50%' }}>
+              <TextField
+                label="Massa"
+                value={log.weight || loadingMessage}
+                sx={{ mr: 4 }}
+                InputLabelProps={{
+                  style: { color: '#111111' },
+                }}
+                InputProps={{
+                  style: {
+                    borderRadius: '8px',
+                    color: '#111111',
+                  },
+                }}
+              />
+              <TextField
+                label="Tamanho"
+                name="length"
+                value={log.length || loadingMessage}
+                InputLabelProps={{
+                  style: { color: '#111111' },
+                }}
+                InputProps={{
+                  style: {
+                    borderRadius: '8px',
+                    color: '#111111',
+                  },
+                }}
+              />
             </Box>
           </Box>
-          <Box sx={{ mt: 1, display: 'flex', flexDirection: 'column'}}>
+          <Box sx={{ mt: 1, display: 'flex', flexDirection: 'column' }}>
             <Typography>Foto</Typography>
-            <img src="https://pbs.twimg.com/profile_images/1529487153402724352/ORsFoDlm_400x400.jpg" width={350} height={200}/>
-            <Typography sx={{ mt: 5}}>Localização</Typography>
-            <div className="mapouter">
-              <div className="gmap_canvas">
-                <iframe width="350" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed">
-                  </iframe><a href="https://fmovies-online.net"></a></div>
-                    </div>
+            <img src={`data:image/png;base64,${log.photo}`} width={350} height={200} />
+            <Typography sx={{ mt: 5 }}>Localização</Typography>
+
+            {!!log.coordenates && (
+              <MapContainer
+                center={[log.coordenates.latitude, log.coordenates.longitude]}
+                zoom={13}
+                scrollWheelZoom
+                style={{ height: '40vh' }}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[log.coordenates.latitude, log.coordenates.longitude]}>
+                  <Popup>Esta é a localização do {log.name}</Popup>
+                </Marker>
+              </MapContainer>
+            )}
           </Box>
         </Box>
       </Grid>
