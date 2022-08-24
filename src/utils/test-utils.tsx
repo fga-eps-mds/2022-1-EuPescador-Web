121c407f2280968ba1043f1bc43b4c8107eba00e
Bruno Henrique <bh.bsb2011@hotmail.com>
2022-08-23 21:49:20 -0300

    remove estilos n usados

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,5 +1,4 @@
 import { render as rtlRender, RenderOptions } from '@testing-library/react'
-import GlobalStyle from '~assets/styles/global'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { PropsWithChildren } from 'react'
 
@@ -7,10 +6,7 @@ function render(ui: React.ReactElement, renderOptions?: RenderOptions) {
   function Wrapper({ children }: PropsWithChildren<object>): JSX.Element {
     return (
       <Router>
-          <>
-            <GlobalStyle />
-            {children}
-          </>
+        <>{children}</>
       </Router>
     )
   }
