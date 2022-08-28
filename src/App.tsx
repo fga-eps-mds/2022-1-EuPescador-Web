7cb8a2119e1854238cea9300a0cd29b533972d22
AlefMemTav <190115548@aluno.unb.br>
2022-08-28 10:53:54 -0300

    Removido arquivos de teste

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Login from 'pages/Login'
 import Register from 'pages/Register'
 
 import './assets/styles/Sidebar.css'
-import './assets/styles/Table.css'
 
 import React from 'react'
 import UserForm from './pages/User/UserEdit'
