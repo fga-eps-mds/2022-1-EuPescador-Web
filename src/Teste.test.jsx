f8fb225d7c1dff611766200ca96c27debd948dc5
AlefMemTav <190115548@aluno.unb.br>
2022-09-11 19:57:29 -0300

    Formato de um teste de componente

---
+++ src/Teste.test.jsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+
+describe('Texto tal', () => {
+    
+    it('deve fazer tal', () => {
+        //Codigo
+    })
+
+    it('deve fazer aquilo também', () => {
+        //Codigo
+    })
+})
