92c7f697ad57221ff8f77f6e6e0853a23addce18
AlefMemTav <190115548@aluno.unb.br>
2022-09-16 11:19:12 -0300

    Corrigido validação de administrador para deletar usuário

--- src/services/api/userServices/deleteUser.ts
+++ src/services/api/userServices/deleteUser.ts
@@ -1,17 +1,21 @@
 import { ResI } from '../interfaces'
 import { adminService } from '../adminServices/adminService'
+import { UserProps } from '~components/Header'
 
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 export async function deleteUser(id: string) {
+  const user: UserProps = JSON.parse(localStorage.getItem('UserData')) as UserProps
   try {
-    const userSuperAdmin = localStorage.getItem('@eupescador/userSuperAdmin')
-
-    if (userSuperAdmin === 'true') {
-      const token = 'admToken'
-      const superAdminToken = `Bearer ${token}`
-      const res: ResI = await adminService.delete(`/admin/${id}`, { headers: { Authorization: superAdminToken } })
+    if (user.admin) {
+      const token = user.token
+      const adminToken = `Bearer ${token}`
+      const res: ResI = await adminService.delete(`/user/${id}`, { headers: { Authorization: 
+        adminToken } 
+      })
       return res.status
     } else {
-      console.log('Deu errado')
+      console.log('Usuario nao administrador')
     }
   } catch (error) {
     console.error(error)
