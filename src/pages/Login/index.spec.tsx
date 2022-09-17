c1de72829a2a353bee7ac2040e6f34246dd17282
murilopbs <murilopbsouto@gmail.com>
2022-09-17 12:56:03 -0300

    Testando o textfield da senha

--- src/pages/Login/index.spec.tsx
+++ src/pages/Login/index.spec.tsx
@@ -54,6 +54,20 @@ describe('Login page', () => {
     const style = getComputedStyle(element[0])
     expect(style.backgroundColor).toBe('')
   })
+
+  it('Should render second TextField', () => {
+    render(<Login />)
+    const element = document.getElementsByClassName('textfield')
+    const style = getComputedStyle(element[1])
+    expect(style.visibility).toBe('visible')
+  })
+
+  it('Second TextField background color should be default', () => {
+    render(<Login />)
+    const element = document.getElementsByClassName('textfield')
+    const style = getComputedStyle(element[1])
+    expect(style.backgroundColor).toBe('')
+  })
 })
 
-export {}
+export { }
