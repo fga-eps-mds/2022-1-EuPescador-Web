f67087816ae3b2da9e38c7fbbe10e2eb090eed9e
oo7gabriel <gabrielfsacademico@gmail.com>
2022-12-18 19:43:36 -0300

    Adicionando mock jest transform no jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -41,6 +41,10 @@ export default {
   //   "lcov",
   //   "clover"
   // ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/mocks/fileMock.js',
+  },
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
