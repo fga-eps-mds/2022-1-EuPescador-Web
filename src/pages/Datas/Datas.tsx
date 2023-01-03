620c6149b0a9aa74b2c6365d7c1c14147d108f8b
emysdias <diassousa@yahoo.com.br>
2023-01-02 23:57:29 -0300

    Ajuste das telas

--- src/pages/Datas/Datas.tsx
+++ src/pages/Datas/Datas.tsx
@@ -4,22 +4,28 @@ import { useState, useEffect } from 'react'
 import { Grid, Box, Modal } from '@mui/material'
 
 // Components
-import Header from '../../components/Header'
-import Sidebar from '../../components/Sidebar'
-import { FishCard } from '../../components/FishCard/FishCard'
-import { FishModal } from '../../components/FishModal/FishModal'
-import { TitlePage } from '../../components/TitlePage/TitlePage'
+import Header from '~components/Header'
+import Sidebar from '~components/Sidebar'
+import { FishCard } from '~components/FishCard/FishCard'
+import { FishModal } from '~components/FishModal/FishModal'
+import { TitlePage } from '~components/TitlePage/TitlePage'
 
 // Services
-import { GetWikiFishes } from '../../services/api/wikiServices/getWikiFishes'
-import { FishWiki } from '../../services/api/interfaces'
+import { GetWikiFishes } from '~services/api/wikiServices/getWikiFishes'
+import { FishWiki } from '~services/api/interfaces'
 
 const Datas = () => {
   const [open, setOpen] = useState(false)
   const [fishes, setFishes] = useState([] as FishWiki[])
   const [modalFish, setModalFish] = useState({} as FishWiki)
 
-  const handleOpen = () => setOpen(true)
+  const handleOpen = () => {
+    setOpen(true)
+    const fishModal = document.getElementById('fishEditModal')
+    const fishModalBack = document.getElementById('fishEditModalBackground')
+    if (fishModal) fishModal.style.visibility = 'visible'
+    if (fishModalBack) fishModalBack.style.visibility = 'visible'
+  }
   const handleClose = () => setOpen(false)
 
   useEffect(() => {
@@ -38,7 +44,7 @@ const Datas = () => {
     <>
       <Grid container>
         <Header />
-        <Grid item xs={2} md={1} >
+        <Grid item xs={2} md={1}>
           <Sidebar children={undefined} />
         </Grid>
         <Grid item xs={10} md={11}>
@@ -49,12 +55,17 @@ const Datas = () => {
               return (
                 <Grid item xs={12} sm={6} md={4} xl={3} key={index}>
                   <Box
-                    onClick={() => { handleOpenFishModal(res) }}
+                    onClick={() => {
+                      handleOpenFishModal(res)
+                    }}
                   >
                     <FishCard
                       fish={{
                         name: res.commonName,
-                        imageUrl: res.photo == null ? 'https://source.unsplash.com/qsHDqcJzHOA' : res.photo,
+                        imageUrl:
+                          res.photo == null
+                            ? 'https://source.unsplash.com/qsHDqcJzHOA'
+                            : res.photo,
                       }}
                     ></FishCard>
                   </Box>
@@ -65,14 +76,9 @@ const Datas = () => {
         </Grid>
       </Grid>
 
-      <Modal
-        open={open}
-        onClose={handleClose}
-      >
+      <Modal id="fishEditModalBackground" open={open} onClose={handleClose}>
         <Box>
-          <FishModal
-            fish={modalFish}
-          ></FishModal>
+          <FishModal fish={modalFish}></FishModal>
         </Box>
       </Modal>
     </>
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -12,14 +12,13 @@ import {
 import Pagination from '@mui/material/Pagination'
 
 import Header, { UserProps } from '~components/Header'
-import Sidebar from '../../components/Sidebar'
+import Sidebar from '~components/Sidebar'
 import TableComponent from '~components/Table'
+import { TitlePage } from '~components/TitlePage/TitlePage'
 import { GetAllUsers, UserResponseI } from '~services/api/userServices/getAllUsers'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import "../../assets/styles/User.css"
-
-import fishIcon from "../../assets/icons/peixe_simbolo1.svg"
+import "~assets/styles/User.css"
 
 export default function User() {
   const navigate = useNavigate()
@@ -71,10 +70,7 @@ export default function User() {
         <Sidebar children={undefined} />
       </Grid>
       <Grid item xs={11}>
-        <h2 style={{marginBottom: '20px', display: 'flex', alignItems: 'center'}}>
-        <img src={fishIcon} style={{width: "50px", height: "70px", marginRight:'8px'}}/>
-        Gerência de Usuários
-        </h2>
+        <TitlePage title="Gerência de Usuários" />
         {users && users.data.length ? (
           <>
           <TableComponent
